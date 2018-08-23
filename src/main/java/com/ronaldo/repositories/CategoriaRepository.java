07539b8a429c2a74499b413637a87b3d92ffff24
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-08-22 23:53:38 -0300

    Criando repository e service para a categoria

---
+++ src/main/java/com/ronaldo/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.ronaldo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldo.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+
+}
---
+++ src/main/java/com/ronaldo/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.ronaldo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronaldo.domain.Categoria;
+import com.ronaldo.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+		
+	}
+}
