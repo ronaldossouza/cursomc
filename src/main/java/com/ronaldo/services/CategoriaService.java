f9e57d7bb88af2012fb21707847deee04f83b439
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-08-25 00:54:55 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/ronaldo/services/CategoriaService.java
+++ src/main/java/com/ronaldo/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.ronaldo.domain.Categoria;
 import com.ronaldo.repositories.CategoriaRepository;
+import com.ronaldo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,6 +14,10 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado. Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 		
 	}
