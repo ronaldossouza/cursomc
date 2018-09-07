ef7813cbcbf4cec891fe25cf36f9fa8735b9ea4f
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-07 00:36:31 -0300

    Cliente tipo cliente telefones e enderecos

---
+++ src/main/java/com/ronaldo/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.ronaldo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldo.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+
+}
---
+++ src/main/java/com/ronaldo/repositories/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.ronaldo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldo.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+
+}
