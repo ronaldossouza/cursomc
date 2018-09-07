f44a06111632a329cb686f46ef532f31c5b5ddb0
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-07 13:15:43 -0300

    Listando todas as categorias com DTO

---
+++ src/main/java/com/ronaldo/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+package com.ronaldo.dto;
+
+import java.io.Serializable;
+
+import com.ronaldo.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+
+	public CategoriaDTO() {
+
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
--- src/main/java/com/ronaldo/services/CategoriaService.java
+++ src/main/java/com/ronaldo/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.ronaldo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,4 +46,8 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categori que possui produtos");
 			}
 		}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 }
