7832bff33a5949b8d1b83cb9b6b4e675c3ee096d
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-07 00:48:17 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/ronaldo/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.ronaldo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronaldo.domain.Cliente;
+import com.ronaldo.repositories.ClienteRepository;
+import com.ronaldo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado. Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+		
+	}
+}
