55328ad17c1c24ec71cef26eacad25e4070a0ac8
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-07 21:32:35 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/ronaldo/services/ClienteService.java
+++ src/main/java/com/ronaldo/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.ronaldo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ronaldo.domain.Cliente;
+import com.ronaldo.dto.ClienteDTO;
 import com.ronaldo.repositories.ClienteRepository;
+import com.ronaldo.services.exceptions.DataIntegrityException;
 import com.ronaldo.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +29,39 @@ public class ClienteService {
 		return obj;
 		
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+	}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma porque há entidades relacionadas");
+			}
+		}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private  void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
