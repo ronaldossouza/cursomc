cb344e2bb0e254cb546b7522de18cb44cde0f95d
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-07 21:04:48 -0300

    Personalizando o retorno da validação

--- src/main/java/com/ronaldo/resources/exception/StandardError.java
+++ src/main/java/com/ronaldo/resources/exception/StandardError.java
@@ -3,11 +3,14 @@ package com.ronaldo.resources.exception;
 import java.io.Serializable;
 
 public class StandardError implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
+	
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
+	
+	
 	public StandardError(Integer status, String msg, Long timeStamp) {
 		super();
 		this.status = status;
