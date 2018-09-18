17b9311737e0a919679211108c762db14ded2997
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-18 20:59:39 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/ronaldo/config/DevConfig.java
+++ src/main/java/com/ronaldo/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ronaldo.services.DBService;
+import com.ronaldo.services.EmailService;
+import com.ronaldo.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,6 +32,10 @@ public class DevConfig {
 		return true;
 		
 		
+	}
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
