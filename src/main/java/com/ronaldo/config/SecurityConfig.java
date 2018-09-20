b2481e22e500b837bf7a70225100d7621c77edba
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-19 21:22:30 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/ronaldo/config/SecurityConfig.java
+++ src/main/java/com/ronaldo/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.ronaldo.security.JWTAuthenticationFilter;
+import com.ronaldo.security.JWTUtil;
 
 
 @Configuration
@@ -23,6 +28,16 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private Environment env;
+	
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	
 	private static final String[] PUBLIC_MATCHER = {
 			"/h2-console/**",
 		
@@ -38,6 +53,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		
+		
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
@@ -47,7 +63,13 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHER_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHER).permitAll()
 		.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
---
+++ src/main/java/com/ronaldo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ronaldo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ronaldo.domain.Cliente;
+import com.ronaldo.repositories.ClienteRepository;
+import com.ronaldo.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+
+		
+		
+		
+			return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+			
+			
+	}
+
+}
