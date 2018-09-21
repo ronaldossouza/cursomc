2dca831bfa81390cecdbbd158c6981f6827f102b
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-09-20 23:25:23 -0300

    Esqueci a senha

--- src/main/java/com/ronaldo/config/SecurityConfig.java
+++ src/main/java/com/ronaldo/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHER_POST = {
 			"/clientes/**",
-			"/categorias/**"
+			"/auth/forgot/**"
 						
 	};
 	
