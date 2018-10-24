780c20461889ad4a0140ad509f3cdef2b5c8005e
Ronaldo Souza <ronaldo.ssouzasilva@gmail.com>
2018-10-24 19:35:28 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/ronaldo/services/ClienteService.java
+++ src/main/java/com/ronaldo/services/ClienteService.java
@@ -53,6 +53,10 @@ private S3Service s3Service;
 @Value("${img.prefix.client.profile}")
 private String prefix;
 
+
+@Value("${img.profile.size}")
+private Integer size;
+
 public Cliente find(Integer id) {
 
 	UserSS user = UserService.authenticated();
@@ -137,7 +141,12 @@ public Cliente find(Integer id) {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile); 
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
